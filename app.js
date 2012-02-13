db8a10ccefdfade3e5b2cb290393ed7ed84b4d65
xqzhang <xqzhang@thoughtworks.com>
2012-02-13 18:07:37 +0800

    Use port by env PORT.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , util = require('util')
 
 var app = module.exports = express.createServer();
 
@@ -31,5 +32,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
