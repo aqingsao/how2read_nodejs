9356bb4244d4b86b4fb576fdb2296378351f555b
xqzhang <xqzhang@thoughtworks.com>
2012-02-14 16:50:44 +0800

    Use sqlite3 memory database to make it run.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
 var util = require('util');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function(error){
+	if(error){
+		throw error;
+	}
+	else{
+		console.log("Database is created successfully.");
+	}
+});
+db.serialize(function(){
+	db.run("CREATE TABLE Terms(id LONG, name TEXT, reading TEXT, source TEXT)", function(error){
+		if(error){
+			throw error;
+		}
+		else{
+			console.log("Table terms is created.");
+		}
+	});
+	var stmt = db.prepare("insert into Terms(name, reading, source) values(?, ?, ?)");
+	stmt.run('Apache', "ə'pætʃi", "http://apache.com");
+	stmt.run('Maven', "'meiven", "http://apache.com");
+	stmt.finalize();
+});
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){	
-	var terms = [{name: 'Apache', reading: "ə'pætʃi", from: "http://apache.com"}, {name: 'Maven', reading: "'meiven", from: "http://apache.com"}];
-	res.render('index', { title: 'How to read me', terms: terms})
+	// var terms = [{name: 'Apache', reading: "ə'pætʃi", from: "http://apache.com"}, {name: 'Maven', reading: "'meiven", from: "http://apache.com"}];		
+	db.all("SELECT * FROM Terms", function(err, rows) {
+    	console.log(util.inspect(rows));
+		res.render('index', { title: 'How to read me', terms: rows})
+	});
 };
