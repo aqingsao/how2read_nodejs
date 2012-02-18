3439e0132c115f4425464a4b251c5236866fa04b
xqzhang <xqzhang@thoughtworks.com>
2012-02-18 16:54:32 +0800

    Use progressive enhancement to improve ui.

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,40 @@
-stats = function(){
-	// dataset:{correct: 29, wrong: 75}
-	function drawPie(id, data){
-		var total = 0;
-		var percentage = [];
-		for(var i in data){
-			data[i] = data[i] ? parseInt(data[i]) : 0;
-			total += data[i];
-		}
-		total = total == 0 ? 1 : total;
-		for(var i in data){
-			percentage[i] = Math.round(data[i]/total*10000)/100.00+"%"; 
-		}
-		var tooltips = "Total: " + total + "\n" + "Wrong: " + data[0];
+$(function(){
+	$("div.term").each(function(){
+		var rightCount = $(this).find("label span.right").text();
+		var wrongCount = $(this).find("label span.wrong").text();		
+		var statsContainer = $(this).find("div.statscontainer");
+		var id = "term" + $(this).attr('id');
+		statsContainer.empty();
+		statsContainer.append("<canvas id=" + id + "></canvas>");
+		_drawPie(id, parseInt(wrongCount), parseInt(rightCount));
+	});
+	$("div.term form.report").submit(function(){
+		var term = $(this).parents("div.term")
+		var that = $(this);
+		$.post(that.attr('action'), that.serialize(), function(){
+			_updateTerm(term);
+		})
+		return false;
+	});
+	
+	function _updateTerm(term){
+		term.find("input[type='submit']").attr('disabled', true);
+		var canvas = term.find("canvas");
+		var id = term.attr('id');
+		$.getJSON("/term/" + id, function(data){
+			_drawPie(canvas.attr("id"), parseInt(data.wrong_count), parseInt(data.right_count));
+		});
+	}
+	function _toPercent(data, total){
+		return Math.round(data/total*10000)/100.00+"%";
+	}
+	function _drawPie(id, wrongCount, rightCount){
+		var total = wrongCount + rightCount;
+		total = Math.max(total, 1);
 		
-		var pie2 = new RGraph.Pie(id, data); // Create the pie object
+		var tooltips = "Total: " + total + "\n" + "Wrong: " + wrongCount;
+		
+		var pie2 = new RGraph.Pie(id, [wrongCount, rightCount]); // Create the pie object
 		pie2.Set('chart.gutter.left', 10);
 		pie2.Set('chart.gutter.right', 10);
 		pie2.Set('chart.gutter.top', 10);
@@ -25,42 +46,12 @@ stats = function(){
 		pie2.Set('chart.shadow.offsetx', 0);
 		pie2.Set('chart.shadow.offsety', 0);
 		pie2.Set('chart.shadow.blur', 25);
-		pie2.Set('chart.labels', ['Wrong(' + percentage[0] + ')', 'Correct(' + percentage [1]+ ')'])
+		pie2.Set('chart.labels', ['Wrong(' + _toPercent(wrongCount, total) + ')', 'Correct(' + _toPercent(rightCount, total) + ')'])
 		pie2.Set('chart.tooltips', [tooltips, tooltips])
 		pie2.Set('chart.tooltips.event', 'onmousemove')
 		pie2.Set('chart.labels.sticks', true);
 		
 		RGraph.Clear(pie2.canvas);
 		pie2.Draw();
-	}
-	return {
-		drawPie: drawPie
-	}
-}();
-
-$(function(){
-	$("div.term canvas").each(function(){
-		_drawPie($(this));
-	});
-	$("div.term form.report").submit(function(){
-		var that = $(this);
-		$.post(that.attr('action'), that.serialize(), function(){
-			_updateTerm(that.parents("div.term"));
-		})
-		return false;
-	});
-	
-	function _drawPie(canvas){
-		stats.drawPie(canvas.attr('id'), [canvas.attr('wrongcount'), canvas.attr('rightcount')]);	
-	};
-	function _updateTerm(term){
-		term.find("input[type='submit']").attr('disabled', true);
-		var canvas = term.find("canvas");
-		var id = term.find("input[name='id']").val();
-		$.getJSON("/term/" + id, function(data){
-			canvas.attr("wrongCount", data.wrong_count);
-			canvas.attr("rightcount", data.right_count);
-			_drawPie(canvas);
-		});
 	}
 });
