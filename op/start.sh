5263b415ff5c4628d968ca9f95de04acbcf71c74
EC2 Default User <ec2-user@ip-10-212-101-85.ec2.internal>
2012-02-23 15:35:14 +0000

    Use node to start app rather than forever.

--- op/start.sh
+++ op/start.sh
@@ -1,4 +1,5 @@
 export PORT=80
 export NODE_ENV=production
 LOG=/home/ec2-user/h2r/logs
-forever start -a -l $LOG/forever.log -o $LOG/out.log -e $LOG/err.log /home/ec2-user/how2read/app.js
+#forever start -a -l $LOG/forever.log -o $LOG/out.log -e $LOG/err.log /home/ec2-user/how2read/app.js
+node /home/ec2-user/how2read/app.js &
