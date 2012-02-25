93df570ffadcedce648c7ab84236158e2e0dac23
xqzhang <xqzhang@thoughtworks.com>
2012-02-25 15:06:56 +0800

    Use commander to refactor migration.js

---
+++ db/migration.js
@@ -0,0 +1,77 @@
+var util = require('util')
+, sqlite3 = require('sqlite3').verbose()
+, config = require('../config')
+, fs = require('fs');
+
+var scriptPath = "";
+var filePattern = /.*\/?(\d+)_*/;
+var sqlPattern = /\w+/;
+exports.migrate = function(dbname, dir){
+	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
+	_migrateDirectory(db, dir + "/migration/", function(a, b){return a.match(filePattern)[1] - b.match(filePattern)[1]});
+}
+
+exports.rollback = function(dbname, dir){
+	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
+	_migrateDirectory(db, dir + "/rollback/", function(a, b){return b.match(filePattern)[1] - a.match(filePattern)[1]});
+}
+
+exports.createdb = _createdb;
+
+function _createdb(dbname){
+	return new sqlite3.Database(dbname, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function(error){
+		if(error){
+			console.log("Could not open db " + dbname);
+			throw error;
+		}
+		else{
+			console.log("Database " + dbname + " is opened successfully.");
+		}
+	});
+}
+function _migrateDirectory(db, dir, sortby){
+	fs.stat(dir, function(err, stats){
+		if(err){
+			throw err;
+		}
+		if(stats.isDirectory()){
+			fs.readdir(dir, function(err, files){
+				if(err){
+					throw err;
+				}
+				files.sort(sortby);
+				_migrateFiles(db, dir, files);
+			});	
+		}
+		else if(stats.isFile()){
+			_migrateFiles(db, null, [dir]);
+		}
+		else{
+			throw "Illegal directory " + dir;
+		}
+	});
+}
+function _migrateFiles(db, dir, files){
+	db.serialize(function(){
+		for(var i in files){
+			var file = dir ? dir + "/" + files[i] : files[i];
+			var content = fs.readFileSync(file, "utf-8");
+			console.log("Begin to migrate " + files[i]);
+			_migrateFile(db, content);
+		}
+	});
+}
+function _migrateFile(db, sqls){
+	sqls = sqls.split(";");
+	for(var i in sqls){
+		var sql = sqls[i];
+		if(sql.match(sqlPattern)){
+			console.log("sql: " + sql);
+			db.run(sql, function(err){
+				if(err){
+					throw err;
+				}
+			});
+		}
+	}
+}
--- migration.js
+++ migration.js
@@ -1,77 +1,28 @@
-var util = require('util')
-, sqlite3 = require('sqlite3').verbose()
-, config = require('./config')
-, fs = require('fs');
+var migration = require('./db/migration'), 
+config = require('./config'), 
+util = require('util'),
+program = require('commander');
 
-var scriptPath = "";
-var filePattern = /.*\/?(\d+)_*/;
-var sqlPattern = /\w+/;
-exports.migrate = function(dbname, dir){
-	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
-	_migrateDirectory(db, dir + "/migration/", function(a, b){return a.match(filePattern)[1] - b.match(filePattern)[1]});
+program.version('0.0.1')
+.option('-r, --rollback', 'Rollback scripts')
+.option('-m, --migrate', 'Migrate scripts')
+.option('-e, --env <env>', '[optional] default env is development', 'development')
+.parse(process.argv);
+
+var env = program.env;
+var dir = config[env].script;
+
+if(!program.rollback && !program.migrate){
+  console.log("Usage: node migration -r -m [-e env] -h");
+  return;
 }
 
-exports.rollback = function(dbname, dir){
-	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
-	_migrateDirectory(db, dir + "/rollback/", function(a, b){return b.match(filePattern)[1] - a.match(filePattern)[1]});
+var db = migration.createdb(config[env].db);
+if(program.rollback){
+  console.log("Start to rollback scripts on env " + env);
+  migration.rollback(db, dir);   
 }
-
-exports.createdb = _createdb;
-
-function _createdb(dbname){
-	return new sqlite3.Database(dbname, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function(error){
-		if(error){
-			console.log("Could not open db " + dbname);
-			throw error;
-		}
-		else{
-			console.log("Database " + dbname + " is opened successfully.");
-		}
-	});
-}
-function _migrateDirectory(db, dir, sortby){
-	fs.stat(dir, function(err, stats){
-		if(err){
-			throw err;
-		}
-		if(stats.isDirectory()){
-			fs.readdir(dir, function(err, files){
-				if(err){
-					throw err;
-				}
-				files.sort(sortby);
-				_migrateFiles(db, dir, files);
-			});	
-		}
-		else if(stats.isFile()){
-			_migrateFiles(db, null, [dir]);
-		}
-		else{
-			throw "Illegal directory " + dir;
-		}
-	});
-}
-function _migrateFiles(db, dir, files){
-	db.serialize(function(){
-		for(var i in files){
-			var file = dir ? dir + "/" + files[i] : files[i];
-			var content = fs.readFileSync(file, "utf-8");
-			console.log("Begin to migrate " + files[i]);
-			_migrateFile(db, content);
-		}
-	});
-}
-function _migrateFile(db, sqls){
-	sqls = sqls.split(";");
-	for(var i in sqls){
-		var sql = sqls[i];
-		if(sql.match(sqlPattern)){
-			console.log("sql: " + sql);
-			db.run(sql, function(err){
-				if(err){
-					throw err;
-				}
-			});
-		}
-	}
+if(program.migrate){
+  console.log("Start to migrate scripts on env " + env);
+  migration.migrate(db, dir);
 }
