f42f3f44dd3f102805c4bde609955a7bc11b182d
xqzhang <xqzhang@thoughtworks.com>
2012-02-26 20:25:19 +0800

    Use tooltip to vote

--- public/js/main.js
+++ public/js/main.js
@@ -21,88 +21,25 @@ $(function(){
 		$("#" + id).show();
 	});
 		
-	$(".votable").mouseover(function(){
+	$(".term .votable").mouseover(function(){
 		$(this).find("audio").get(0).play();
-	});
-	
-	$("div.term .votable").click(function(){
-		var that = $(this);
-		if(that.hasClass('voted')){
+		if($(this).hasClass('voted')){
 			return;
 		}
 		
-		var term = $(this).parents("div.term");
-		term.find(".votable").each(function(){
-			if(!$(this).hasClass('voted')){
-				$(this).addClass('voted');
-			};
+		var tid = $(this).parents("div.term").attr('id'), rid = $(this).attr('reading');
+		var voting = $("<a class='voting' href='#' onclick='_vote(this); return false;' tid='" + tid + "' rid='" + rid + "'>我就是这么读的</a>")[0];
+		$(this).poshytip({
+			className: 'tip-darkgray',
+			bgImageFrameSize: 9, 
+			showTimeout: 1000,
+			alignTo: 'target',
+			alignX: 'center',
+			alignY: 'bottom', 
+			offsetY: -5,
+			content: voting
 		});
-
-		$.post('/term/' + term.attr('id') + '/reading/' + that.attr('reading'), function(data){
-			_updateTerm(term, that.attr('reading'), data);
-		}).error(function(data){
-			if(term.find("a.reading.error, a.reding.right, a.reading.wrong").length == 0){
-				$("<a class='reading error'>投票错误</a>").insertAfter(term.find('label.rate'));
-			}
-			
-			if(term.find("a.reding.right, a.reading.wrong").length == 0){
-				term.find(".votable").each(function(){
-					if($(this).hasClass('voted')){
-						$(this).removeClass('voted');
-					};
-				});
-			}
-		});
-		
-		return false;
 	});
-	function _updateTerm(term, voted, data){
-		term.find("a.reading.error").each(function(){
-			$(this).detach();
-		});
-		
-		term.find(".votable").each(function(){
-			var reading = $(this).attr('reading');
-			if(!_isCorrect(reading, data.readings)){
-				$(this).detach();
-			}
-		});
-		for(var i in data.readings){
-			if(data.readings[i].id == voted){
-				if(data.readings[i].correct == 'true'){
-					$("<a class='reading right'>您读对了</a>").insertAfter(term.find('label.rate'));
-				}
-				else{
-					$("<a class='reading wrong'>您读错了</a>").insertAfter(term.find('label.rate'));
-				}
-			}
-		}
-		
-		term.find("label.rate span").text(_toPercent(data.wrong, data.right));
-		_drawPie(term.find("canvas").attr("id"), parseInt(data.wrong), parseInt(data.right));
-	}
-	function _drawPie(id, wrongCount, rightCount){
-		var total = Math.max(wrongCount + rightCount, 1);
-		
-		var pie2 = new RGraph.Pie(id, [wrongCount, rightCount]); // Create the pie object
-		pie2.Set('chart.gutter.left', 20);
-		pie2.Set('chart.gutter.right', 20);
-		pie2.Set('chart.gutter.top', 35);
-		pie2.Set('chart.gutter.bottom', 20);
-		pie2.Set('chart.colors', ['#FFAA52', '#3E6D8E']);
-		pie2.Set('chart.strokestyle', 'white');
-		pie2.Set('chart.shadow', true);
-		pie2.Set('chart.shadow.offsetx', 0);
-		pie2.Set('chart.shadow.offsety', 0);
-		pie2.Set('chart.shadow.blur', 25);
-		pie2.Set('chart.tooltips', [wrongCount + " wrong", rightCount + " right"]);
-		pie2.Set('chart.tooltips.event', 'onmousemove');
-		pie2.Set('chart.title', total + ' votes');
-		pie2.Set('chart.title.bold', false);
-		
-		RGraph.Clear(pie2.canvas);
-		pie2.Draw();
-	};
 	$(".share a").click(function(){
 		var term = $(this).parents('div.term');
 		var word = term.find(".summary h2").text();
@@ -110,17 +47,6 @@ $(function(){
 		jiathis_config.summary = word + "，据统计，" + rate + "的人读错了这个单词，你呢？";
 		jiathis_config.url = "http://how2read.me#"+word;
 	});
-	$(".term .votable").poshytip({
-		className: 'tip-darkgray',
-		bgImageFrameSize: 9, 
-		showTimeout: 1000,
-		alignTo: 'target',
-		alignX: 'center',
-		alignY: 'bottom', 
-		offsetY: -5,
-		content: '<a>我就是这么读的</a>'
-	});
-	$(".term .votable.voted").poshytip('destroy');
 });
 function getRate(wrongCount, rightCount){
 	var total = wrongCount + rightCount;
@@ -144,4 +70,77 @@ function _isCorrect(reading, readings){
 	}
 	return false;
 }
+function _vote(reading){
+	var that = $(reading);
+	
+	var term = $("div#" + that.attr('tid') + ".term");
+	term.find(".votable").each(function(){
+		$(this).poshytip('disable');
+	});
+
+	$.post('/term/' + that.attr('tid') + '/reading/' + that.attr('rid'), function(data){
+		_updateTerm(term, that.attr('rid'), data);
+	}).error(function(data){
+		if(term.find("a.reading.error, a.reding.right, a.reading.wrong").length == 0){
+			$("<a class='reading error'>投票错误</a>").insertAfter(term.find('label.rate'));
+		}
+		
+		if(term.find("a.reading.right, a.reading.wrong").length == 0){
+			term.find(".votable").each(function(){
+				if($(this).hasClass('voted')){
+					$(this).removeClass('voted');
+				};
+			});
+		}
+	});
+	
+	return false;
+}
+function _updateTerm(term, voted, data){
+	term.find("a.reading.error").each(function(){
+		$(this).detach();
+	});
+	
+	term.find(".votable").each(function(){
+		var reading = $(this).attr('reading');
+		if(!_isCorrect(reading, data.readings)){
+			$(this).detach();
+		}
+	});
+	for(var i in data.readings){
+		if(data.readings[i].id == voted){
+			if(data.readings[i].correct == 'true'){
+				$("<a class='reading right'>您读对了</a>").insertAfter(term.find('label.rate'));
+			}
+			else{
+				$("<a class='reading wrong'>您读错了</a>").insertAfter(term.find('label.rate'));
+			}
+		}
+	}
+	
+	term.find("label.rate span").text(_toPercent(data.wrong, data.right));
+	_drawPie(term.find("canvas").attr("id"), parseInt(data.wrong), parseInt(data.right));
+}
+function _drawPie(id, wrongCount, rightCount){
+	var total = Math.max(wrongCount + rightCount, 1);
+	
+	var pie2 = new RGraph.Pie(id, [wrongCount, rightCount]); // Create the pie object
+	pie2.Set('chart.gutter.left', 20);
+	pie2.Set('chart.gutter.right', 20);
+	pie2.Set('chart.gutter.top', 35);
+	pie2.Set('chart.gutter.bottom', 20);
+	pie2.Set('chart.colors', ['#FFAA52', '#3E6D8E']);
+	pie2.Set('chart.strokestyle', 'white');
+	pie2.Set('chart.shadow', true);
+	pie2.Set('chart.shadow.offsetx', 0);
+	pie2.Set('chart.shadow.offsety', 0);
+	pie2.Set('chart.shadow.blur', 25);
+	pie2.Set('chart.tooltips', [wrongCount + " wrong", rightCount + " right"]);
+	pie2.Set('chart.tooltips.event', 'onmousemove');
+	pie2.Set('chart.title', total + ' votes');
+	pie2.Set('chart.title.bold', false);
+	
+	RGraph.Clear(pie2.canvas);
+	pie2.Draw();
+};
 
