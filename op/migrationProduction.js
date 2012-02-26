b832863e1c534aafdfb56c474381dee3ae0072a1
EC2 Default User <ec2-user@ip-10-212-101-85.ec2.internal>
2012-02-26 14:34:07 +0000

    Change db directory

--- op/migrationProduction.js
+++
@@ -1,7 +0,0 @@
-var migration = require('../migration');
-var config = require('../config');
-
-var env = 'production';
-var dbname = config[env].db;
-var dir = config[env].script;
-migration.migrate(dbname, dir);
--- op/start.sh
+++ op/start.sh
@@ -1,5 +1,5 @@
 export PORT=80
 export NODE_ENV=production
-LOG=/home/ec2-user/h2r/logs
-#forever start -a -l $LOG/forever.log -o $LOG/out.log -e $LOG/err.log /home/ec2-user/how2read/app.js
-node /home/ec2-user/how2read/app.js &
+LOG=/home/ec2-user/logs
+forever start -a -l $LOG/forever.log -o $LOG/out.log -e $LOG/err.log /home/ec2-user/how2read/app.js
+#node /home/ec2-user/how2read/app.js &
