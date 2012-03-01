47e9e8343abb7a577bb2061393efeb481f2ac802
xqzhang <xqzhang@thoughtworks.com>
2012-03-01 18:29:18 +0800

    Add migration scripts for new terms.

---
+++ db/release02/migration/001_insert_table_terms.sql
@@ -0,0 +1,34 @@
+insert into Terms(name, source, description) values('ubuntu', "http://www.ubuntu.com", "以桌面应用为主的GNU/Linux操作系统。");
+insert into readings(symbol, audio, is_correct, term) values("u:'bu:ntu:", 'ubuntu_wubantu.mp3', 'false', (select id from terms where name='ubuntu'));
+insert into readings(symbol, audio, is_correct, term) values("'ju:bu:ntu:", 'ubuntu_ubangtu.mp3', 'true', (select id from terms where name='ubuntu'));
+
+insert into Terms(name, source, description) values('GUI', "", "图形用户界面，是指采用图形方式显示的计算机操作用户界面。");
+insert into readings(symbol, audio, is_correct, term) values("'guˌi:", 'ubuntu_guyi.mp3', 'true', (select id from terms where name='GUI'));
+insert into readings(symbol, audio, is_correct, term) values("G-U-I", 'ubuntu_zhiyouai.mp3', 'false', (select id from terms where name='GUI'));
+
+insert into Terms(name, source, description) values('Debian', "", "采用GPL协议授权的操作系统，基于Linux内核。");
+insert into readings(symbol, audio, is_correct, term) values("'dibi:n", 'debian_dibin.mp3', 'false', (select id from terms where name='Debian'));
+insert into readings(symbol, audio, is_correct, term) values("'dəbi:n", 'debian_debin.mp3', 'false', (select id from terms where name='Debian'));
+insert into readings(symbol, audio, is_correct, term) values("'dεbi:n", 'debian_daibin.mp3', 'true', (select id from terms where name='Debian'));
+
+insert into Terms(name, source, description) values('WSDL', "", "一门基于XML 的语言，用于描述Web Services 以及如何对它们进行访问。");
+insert into readings(symbol, audio, is_correct, term) values("'weisˌdəu:", 'ubuntu_weisidou.mp3', 'true', (select id from terms where name='GUI'));
+insert into readings(symbol, audio, is_correct, term) values("W-S-D-L", 'ubuntu_wsdl.mp3', 'false', (select id from terms where name='GUI'));
+
+insert into Terms(name, source, description) values('Adobe', "", "图形用户界面，是指采用图形方式显示的计算机操作用户界面。");
+insert into readings(symbol, audio, is_correct, term) values("ə'dəubi", 'ubuntu_edoubi.mp3', 'true', (select id from terms where name='Adobe'));
+insert into readings(symbol, audio, is_correct, term) values("ə'dəub", 'ubuntu_edoubo.mp3', 'false', (select id from terms where name='Adobe'));
+
+insert into Terms(name, source, description) values('Youtube', "", "世界上最大的视频分享网站。");
+insert into readings(symbol, audio, is_correct, term) values("'ju:tju:b", 'youtube_youtiubo.mp3', 'true', (select id from terms where name='Youtube'));
+insert into readings(symbol, audio, is_correct, term) values("ju:'tu:b", 'youtube_youtubo.mp3', 'false', (select id from terms where name='Youtube'));
+
+insert into Terms(name, source, description) values('AJAX', "", "异步JavaScript 及XML，用于创建更好更快以及交互性更强的 Web 应用程序。");
+insert into readings(symbol, audio, is_correct, term) values("'eidʒæks", 'ubuntu_eizhaikes.mp3', 'true', (select id from terms where name='AJAX'));
+insert into readings(symbol, audio, is_correct, term) values("ə'dʒæks", 'ubuntu_ezhaikes.mp3', 'false', (select id from terms where name='AJAX'));
+insert into readings(symbol, audio, is_correct, term) values("a'dʒæks", 'ubuntu_azhaikes.mp3', 'false', (select id from terms where name='AJAX'));
+insert into readings(symbol, audio, is_correct, term) values("a'dʒaks", 'ubuntu_ajiakes.mp3', 'false', (select id from terms where name='AJAX'));
+
+insert into Terms(name, source, description) values('App', "", "应用程序application program的简称。");
+insert into readings(symbol, audio, is_correct, term) values("æp", 'app_aipu.mp3', 'true', (select id from terms where name='App'));
+insert into readings(symbol, audio, is_correct, term) values("A-P-P", 'app_eipipi.mp3', 'false', (select id from terms where name='App'));
---
+++ db/release02/rollback/001_insert_table_terms.js
@@ -0,0 +1,23 @@
+delete from readings where term='ubuntu';
+delete from Terms where name = 'ubuntu';
+
+delete from readings where term='GUI';
+delete from Terms where name = 'GUI';
+
+delete from readings where term='Debian';
+delete from Terms where name = 'Debian';
+
+delete from readings where term='WSDL';
+delete from Terms where name = 'WSDL';
+
+delete from readings where term='Adobe';
+delete from Terms where name = 'Adobe';
+
+delete from readings where term='Youtube';
+delete from Terms where name = 'Youtube';
+
+delete from readings where term='AJAX';
+delete from Terms where name = 'AJAX';
+
+delete from readings where term='App';
+delete from Terms where name = 'App';
