a453b652f2787a7b451115e746748b04b9152ea4
xqzhang <xqzhang@thoughtworks.com>
2012-03-02 17:56:36 +0800

    Make rollback and migrate work

--- db/migration.js
+++ db/migration.js
@@ -1,19 +1,79 @@
 var util = require('util')
 , sqlite3 = require('sqlite3').verbose()
-, config = require('../config')
 , fs = require('fs');
 
+var nodbdeploy_table = "nodeploydb";
+var CREATE_SQL = "CREATE TABLE nodeploydb(id INTEGER, num INTEGER NOT NULL, fileName varchar(100) NOT NULL, created_at timestamp DEFAULT CURRENT_TIMESTAMP, updated_at timestamp DEFAULT CURRENT_TIMESTAMP, CONSTRAINT PK_TERMS_ID PRIMARY KEY(ID));";
 var scriptPath = "";
 var filePattern = /.*\/?(\d+)_*/;
 var sqlPattern = /^\s*\w+/;
-exports.migrate = function(dbname, dir){
+exports.migrate = function(dbname, dir, migrateTo){
 	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
-	_migrateDirectory(db, dir + "/migration/", function(a, b){return a.match(filePattern)[1] - b.match(filePattern)[1]});
+	_getLatestMigrated(db, function(latestScript){
+		var filter = function(script){var num = script.match(filePattern)[1]; return num <= migrateTo && num > latestScript};
+		var sortby = function(a, b){return a.match(filePattern)[1] - b.match(filePattern)[1]};
+		var migrateDir = dir + "/migration/";
+		_lookupScriptsInDirectory(migrateDir, function(files){
+			files = files.filter(filter).sort(sortby);
+
+			console.log("Of them, %s scripts are going to be migrated.", files.length);
+			for(var i in files){
+				console.log("Script %s", files[i]);
+			}
+			
+			
+			db.serialize(function(){
+				for(var i in files){
+					var num = files[i].match(filePattern)[1];
+					var file = migrateDir + "/" + files[i];
+					var content = fs.readFileSync(file, "utf-8");
+					console.log("++Start to migrate " + files[i]);
+					_runSqls(db, content, num, file, function(num, file){
+						db.run("Insert into " + nodbdeploy_table + "(num, FileName) values(?, ?)", num, file, function(err){
+							if(err){
+								console.log("Failed to insert into nodbdeploy meta table for " + file + ": " + util.inspect(err));
+								throw "Failed to insert in nodbdeploy meta table";
+							}
+						});
+					});
+				}
+			});
+		});			
+	});
 }
 
-exports.rollback = function(dbname, dir){
+exports.rollback = function(dbname, dir ,rollbackTo){
 	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
-	_migrateDirectory(db, dir + "/rollback/", function(a, b){return b.match(filePattern)[1] - a.match(filePattern)[1]});
+	_getLatestMigrated(db, function(latestScript){
+		var filter = function(script){var num = script.match(filePattern)[1]; return num > rollbackTo && num <= latestScript};
+		var sortby = function(a, b){return b.match(filePattern)[1] - a.match(filePattern)[1]};
+		var rollbackDir = dir + "/rollback/";
+		_lookupScriptsInDirectory(rollbackDir, function(files){
+			files = files.filter(filter).sort(sortby);
+
+			console.log("Of them, %s scripts are going to be rolled back.", files.length);
+			for(var i in files){
+				console.log("Script %s", files[i]);
+			}
+
+			db.serialize(function(){
+				for(var i in files){
+					var num = files[i].match(filePattern)[1];
+					var file = rollbackDir + "/" + files[i];
+					var content = fs.readFileSync(file, "utf-8");
+					console.log("++Start to rollback " + files[i]);
+					_runSqls(db, content, num, file, function(num, file){
+						db.run("Delete from " + nodbdeploy_table + " where num = ? ", num, function(err){
+							if(err){
+								console.log("Failed to insert in nodbdeploy meta table for " + file + ": " + util.inspect(err));
+								throw "Failed to insert in nodbdeploy meta table";
+							}
+						});
+					});
+				}
+			});
+		});			
+	});
 }
 
 exports.createdb = _createdb;
@@ -29,7 +89,37 @@ function _createdb(dbname){
 		}
 	});
 }
-function _migrateDirectory(db, dir, sortby){
+function _getLatestMigrated(dbname, callback){
+	var db = typeof(dbname) == "string" ? _createdb(dbname) : dbname;
+	db.get("select max(num) as num, fileName from " + nodbdeploy_table, function(err, row){
+		if(err){
+			if(err.message.indexOf("no such table") > 0){
+				console.log("nodbdeploy meta table does not exist, will create %s.", nodbdeploy_table);
+				db.run(CREATE_SQL, function(err){
+					if(err){
+						console.log("Error when create nodbdeploy meta table: %s", util.inspect(err));
+						return;
+					}
+					callback(0);			
+				});
+			}
+			else{
+				console.log("Error when accessing db: %s" + util.inspect(err));
+				return;				
+			}
+		}
+		else{
+			if(row.num){
+			  	console.log("Latest script is " + row.num + ": " + row.fileName);
+				callback(row.num);
+			}
+			else{
+				callback(0);
+			}
+		}
+	});
+}
+function _lookupScriptsInDirectory(dir, callback){
 	fs.stat(dir, function(err, stats){
 		if(err){
 			throw err;
@@ -39,39 +129,37 @@ function _migrateDirectory(db, dir, sortby){
 				if(err){
 					throw err;
 				}
-				files.sort(sortby);
-				_migrateFiles(db, dir, files);
+				console.log("Totally found %s scripts.", files.length);
+				for(var i in files){
+					console.log("Script %s", files[i]);
+				}
+				
+				callback(files);
 			});	
-		}
-		else if(stats.isFile()){
-			_migrateFiles(db, null, [dir]);
 		}
 		else{
 			throw "Illegal directory " + dir;
 		}
 	});
 }
-function _migrateFiles(db, dir, files){
+function _runSqls(db, sqls, num, file, callback){
+	sqls = sqls.split(";");
 	db.serialize(function(){
-		for(var i in files){
-			var file = dir ? dir + "/" + files[i] : files[i];
-			var content = fs.readFileSync(file, "utf-8");
-			console.log("Begin to migrate " + files[i]);
-			_migrateFile(db, content);
+		var count = 0;
+		for(var i in sqls){
+			var sql = sqls[i];
+			if(sql.match(sqlPattern)){
+				// console.log("--sql: " + sql);
+				db.run(sql, function(err){
+					if(err){
+						throw err;
+					}
+				});
+			}
+			count ++;
+			if(count >= sqls.length){
+				callback(num, file);
+			}
 		}
 	});
-}
-function _migrateFile(db, sqls){
-	sqls = sqls.split(";");
-	for(var i in sqls){
-		var sql = sqls[i];
-		if(sql.match(sqlPattern)){
-			console.log("sql: " + sql);
-			db.run(sql, function(err){
-				if(err){
-					throw err;
-				}
-			});
-		}
-	}
 }
--- migration.js
+++ migration.js
@@ -4,25 +4,40 @@ util = require('util'),
 program = require('commander');
 
 program.version('0.0.1')
-.option('-r, --rollback', 'Rollback scripts')
-.option('-m, --migrate', 'Migrate scripts')
-.option('-e, --env <env>', '[optional] default env is development', 'development')
+.option('-r, --rollbackTo [number]', '[optional] Rollback scripts to [number], excluded, default to all', parseInt)
+.option('-m, --migrateTo [number]', '[optional] Migrate scripts to [number], included, default to newest', parseInt)
+.option('-e, --env [env]', '[optional] default env is development', 'development')
+.option('-d, --directory <dir>', "If set to <dir>, then look up migration scripts in <dir>/migration, and lookup rollback scripts in <dir>/rollback, default to 'db'", 'db')
 .parse(process.argv);
 
 var env = program.env;
 var dir = config[env].script;
 
-if(!program.rollback && !program.migrate){
-  console.log("Usage: node migration -r -m [-e env] -h");
+if(!program.rollbackTo && !program.migrateTo){
+  console.log("Usage: node migration <-r [number] | -m [number]> [-e [env]] -h");
   return;
 }
 
+console.log("nodbdeploy runs on env %s, will use %s as base directory. ",env,  program.directory);
+
 var db = migration.createdb(config[env].db);
-if(program.rollback){
-  console.log("Start to rollback scripts on env " + env);
-  migration.rollback(db, dir);   
+if(program.rollbackTo){
+	if(program.rollbackTo == true){
+		console.log("Will rollback all scripts.");
+	  	migration.rollback(db, program.directory, 0);   
+	}
+	else{
+	  	console.log("Will rollback scripts to " + program.migrateTo);
+	  	migration.rollback(db, program.directory, program.rollbackTo);   
+	}
 }
-if(program.migrate){
-  console.log("Start to migrate scripts on env " + env);
-  migration.migrate(db, dir);
+if(program.migrateTo){
+	if(program.migrateTo == true){
+		console.log("Will migrate scripts to newest.");
+		migration.migrate(db, program.directory, 999);
+	}
+	else{
+	  	console.log("Will migrate scripts to " + program.migrateTo);
+	  	migration.migrate(db, program.directory, program.migrateTo);
+	}
 }
