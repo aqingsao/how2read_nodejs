8746ff0ef876b0b08b6996dc1e1c8d5c5c8979d3
xqzhang <xqzhang@thoughtworks.com>
2012-11-01 16:12:26 +0800

    Add backup db file

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 var jiathis_config = {
-    title:"程序员最容易读错的单词：",
+    title:"#程序员最容易读错的单词# ",
     ralateuid:{
         "tsina":"1566959347"
     }
@@ -69,7 +69,7 @@ $(function(){
 		var voted = $("#challenge .score .voted").text();
 		var correct = $("#challenge .score .correct").text();
 		var rate = $("#challenge .score .rate").text();
-		jiathis_config.summary = "我答对了" + voted + "中的" + correct + "个，战胜了" + rate + "的人";
+		jiathis_config.summary = "我答对了" + voted + "中的" + correct + "个，战胜了" + rate + "的人，你也来试试吧";
 		jiathis_config.url = "http://how2read.me";
 	});
 	_updateScore();
